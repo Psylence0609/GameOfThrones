ed37d4eafcf3e80d4098907d1bdd570f6c930edb
Praneet Surabhi <praneetasritha@gmail.com>
2025-04-11 11:05:27 -0500

    Fixed some browser issues

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ import threading
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'secret!'  # Replace with a real secret key in production
-CORS(app)
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 socketio.init_app(app)  # Initialize SocketIO with the app
 
 app.register_blueprint(api_bp, url_prefix='/api')
@@ -16,9 +16,9 @@ app.register_blueprint(api_bp, url_prefix='/api')
 initialize_simulation()
 
 # Start the regular simulation in a separate thread
-simulation_thread = threading.Thread(target=run_simulation_regularly)
-simulation_thread.daemon = True  # Allow the thread to exit when the main thread exits
-simulation_thread.start()
+# simulation_thread = threading.Thread(target=run_simulation_regularly)
+# simulation_thread.daemon = True  # Allow the thread to exit when the main thread exits
+# simulation_thread.start()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5001)
